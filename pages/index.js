b70edb91b99222a1cbc6eeeb5bea8481eabfd1ab
Erlon Carlos de Oliveira Gomes <erlonlon@gmail.com>
2021-04-23 10:40:38 -0300

    Pagína Cotato e Sobre
    Pagína Cotato e Sobre

--- pages/index.js
+++ pages/index.js
@@ -12,28 +12,30 @@ const Index = () => {
     <>
       <PageTitle title='Home' />
 
-      <div className='container mx-auto'>
-        <div className="flex justify-center py-4 ">
-          <div className='bg-gray-800 text-white px-6 py-4 font-bold rounded-lg shadow-lg '>Sistema PalpiteBox</div>
+      <div className='container mx-auto m-4'>
+        <div className="max-w-md mx-auto bg-gray-100 rounded-xl shadow-md overflow-hidden md:max-w-4xl">
+          <div className="flex justify-center py-4 ">
+            <div className='bg-gray-800 text-white px-6 py-4 font-bold rounded-lg shadow-lg '>Sistema PalpiteBox</div>
 
-        </div>
+          </div>
 
-        <div className="flex justify-center py-4 text-center">
-          <p>O Info Automação sempre busca por atender melhor seus clientes.<br></br>
+          <div className="flex justify-center py-4 text-center">
+            <p>O Info Automação sempre busca por atender melhor seus clientes.<br></br>
             Por isso, estamos sempre aberto a ouvir sua opinião.
         </p>
-        </div>
+          </div>
 
-        <div className="flex justify-center p-4 text-center ">
-          {!data && <p>Carregando Cupon...</p>}
-          {!error && data && data.showCoupon &&
-            <p className='px-10 py-4 font-bold bg-red-500 text-white rounded-md shadow-xl '>{data.message}</p>
-          }
-        </div>
-        <div className="flex justify-center py-4 text-center">
-          <Link href='/pesquisa'>
-            <a className='px-6 py-4 font-bold bg-green-600 text-white rounded-lg shadow-lg hover:bg-green-400'>Dar sua opinião ou sugestão</a>
-          </Link>
+          <div className="flex justify-center p-4 text-center ">
+            {!data && <p>Carregando Cupon...</p>}
+            {!error && data && data.showCoupon &&
+              <p className='px-10 py-4 font-bold bg-red-500 text-white rounded-md shadow-xl '>{data.message}</p>
+            }
+          </div>
+          <div className="flex justify-center py-4 text-center">
+            <Link href='/pesquisa'>
+              <a className='px-6 py-4 font-bold bg-green-600 text-white rounded-lg shadow-lg hover:bg-green-400'>Dar sua opinião ou sugestão</a>
+            </Link>
+          </div>
         </div>
       </div>
     </>
--- pages/sobre.js
+++ pages/sobre.js
@@ -8,13 +8,30 @@ const Sobre = () => {
   return (
     <>
       <PageTitle title='Sobre' />
-      <h1>Sobre o Sistema</h1>
-
-      <div>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
+      <div className="container mx-auto px-4 m-4">
+        <div className="max-w-md mx-auto bg-gray-100 rounded-xl shadow-md overflow-hidden md:max-w-4xl">
+          <div className="md:flex">
+            <div className="md:flex-shrink-0">
+              <img className="h-48 w-full object-cover md:w-48" src="/contact.svg" alt="Info Automação" />
+            </div>
+            <div class="p-8">
+              <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Erlon Gomes</div>
+              <a href="/contato"
+                className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+                Desenvolva seu Site ou Sistema na Info Automação</a>
+              <p className="mt-2 text-gray-500">Desenvolvedor de Sistemas Web.</p>
+              <p className="mt-2 text-gray-500">Tecnologias da Stack.</p>
+              <p className="mt-2 text-gray-500">NodeJs.</p>
+              <p className="mt-2 text-gray-500">NextJs.</p>
+              <p className="mt-2 text-gray-500">ReactJS.</p>
+              <p className="mt-2 text-gray-500">Git.</p>
+            </div>
+          </div>
+        </div>
       </div>
+
+
+
     </>
 
 
